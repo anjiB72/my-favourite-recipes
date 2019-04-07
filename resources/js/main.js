b28b9393eaf9117653296b1ef1364454043720bd
anjiB72 <43617582+anjiB72@users.noreply.github.com>
2019-04-07 21:02:14 +0100

    add function to hide dropdown menu when item selected

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,15 @@
 $(function(){
+
+  //Toggle drop down menu when hamburger icon clicked
   $('.container').on('click' , function(e){
     e.preventDefault();
     $('.dropdown-menu').slideToggle();
   });
 
+  //Hide menu when menu item clicked
+  $('.dropdown-menu a').on('click', function(){
+    $('.dropdown-menu').hide();
+  });
 
 
 });
